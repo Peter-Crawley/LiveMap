dd787c94531fa9b603e045bbc7dee2ea7d02859e
Peter Crawley <peterzcrawley@gmail.com>
2022-06-10 14:24:29 +0100

    Region Data

--- src/main/kotlin/LiveMapRegion.kt
+++ src/main/kotlin/LiveMapRegion.kt
@@ -2,6 +2,7 @@ package io.github.petercrawley.livemap
 
 import java.io.File
 import java.io.RandomAccessFile
+import java.nio.channels.FileChannel
 
 internal class LiveMapRegion(
 	regionPosition: Position2D<Short>,
@@ -14,6 +15,8 @@ internal class LiveMapRegion(
 	internal val loadedChunks = mutableListOf<Position2D<Byte>>()
 
 	private val regionFile = RandomAccessFile(worldDirectory.resolve("${regionPosition.x}-${regionPosition.z}.lmr"), "rwd")
+
+	private val data = regionFile.channel.map(FileChannel.MapMode.READ_WRITE, 0, 16*16*32*32*2)
 
 	internal fun close() = regionFile.close()
 }
