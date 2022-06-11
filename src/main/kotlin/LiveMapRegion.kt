e83605730989a75748096bbfd40b00b7b5766791
Peter Crawley <peterzcrawley@gmail.com>
2022-06-11 16:08:01 +0100

    Use _ not -

--- src/main/kotlin/LiveMapRegion.kt
+++ src/main/kotlin/LiveMapRegion.kt
@@ -7,7 +7,7 @@ import java.nio.channels.FileChannel
 internal class LiveMapRegion(regionPosition: Position2D<Short>, worldDirectory: File) {
 	internal val loadedChunks = mutableListOf<Position2D<Byte>>()
 
-	private val regionFile = RandomAccessFile(worldDirectory.resolve("${regionPosition.x}-${regionPosition.z}.lmr"), "rwd")
+	private val regionFile = RandomAccessFile(worldDirectory.resolve("${regionPosition.x}_${regionPosition.z}.lmr"), "rwd")
 	internal val data = regionFile.channel.map(FileChannel.MapMode.READ_WRITE, 0, 16*16*32*32*2)
 
 	init {
